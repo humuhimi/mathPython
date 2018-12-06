9518b58f8e2dbbf8f09659c42897338c3adbec1c
humuhimi <masa19951009@gmail.com>
2018-12-06 22:51:19 +0900

    11章まで終了

---
+++ self_taught_python/hangman.py
@@ -0,0 +1,43 @@
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "________        ",
+             "|               ",
+             "|        |      ",
+             "|        0      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "|               "
+              ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+
+    while wrong < len(stages) -1:
+        print("\n")
+        msg = "文字を１文字予想してね"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print('\n'.join(stages[0:e]))
+        if "__" not in board:
+            print("あなたの勝ち!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("あなたの負け!正解は{0}.".format(word))
+
+
+hangman("cat")
+
+
--- self_taught_python/sample9.py
+++ self_taught_python/sample9.py
@@ -17,6 +17,20 @@ with open("sample.txt","r") as s:
 import csv
 
 with open("sample.scv","w", newline='') as f:
+    w = csv.writer(f,delimiter=",")
+    w.writerow(["one","two","three"])
+    w.writerow(["four","five","six"])
+
+with open("sample.scv","r") as g:
+    r = csv.reader(g,delimiter=",")
+    for row in r:
+        print(",".join(row))
+
+
+
+
+
+
 
 
 
