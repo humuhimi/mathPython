ae0df6818fc9e74c579282b8448e1843bebaf7e3
humuhimi <masa19951009@gmail.com>
2018-12-12 22:08:58 +0900

    practice 2.3

--- mathPython/Chapter1/practice2_3.py
+++ mathPython/Chapter1/practice2_3.py
@@ -1,16 +1,28 @@
 """
 show Expenditure
 """
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import fontManager
+
 categories_num = input('カテゴリー数を入れてください')
-category = []
-expenditure = []
+category = []  # カテゴリーはいる
+expenditure = []  # 費用
 
 while True:
     category.append(input('カテゴリーは何ですか?'))
     expenditure.append(input('費用を入力ください'))
-    if len(expenditure) == int(categories_num): # pythonでは==は型も同じじゃないとあかんのか
-        print('breakできるはず')
+    if len(expenditure) == int(categories_num):  # pythonでは==は型も同じじゃないとあかんのか
         break
+
+plt.rcParams['font.family'] = 'AppleGothic'
+plt.ylim(0,60000)
+plt.title('支出')
+plt.xlabel('カテゴリ')
+plt.ylabel('費用')
+plt.bar(category, expenditure, align='center')
+plt.show()
+
+
 
 
 
