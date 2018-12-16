dbdb0b21074fcb4347f1067ef506abddc97831e8
humuhimi <masa19951009@gmail.com>
2018-12-16 11:43:48 +0900

    lesson3 correlation

--- mathPython/Chapter3/practice3.5.py
+++ mathPython/Chapter3/practice3.5.py
@@ -7,5 +7,25 @@ def find_corr_x_y(x,y):
     sum_prod_x_y = sum(prod)
     sum_x = sum(x)
     sum_y = sum(y)
+    squared_sum_x = sum_x**2
+    squared_sum_y = sum_y**2
+    x_square = []
+    for xi in x:
+        x_square.append(xi**2)
+    # find the sum
+    x_square_sum = sum(x_square)
+    y_square = []
+    for yi in y:
+        y_square.append(yi**2)
+    y_square_sum = sum(y_square)
+
+    # use formula to calculate correlation
+    numerator = n*sum_prod_x_y - sum_x*sum_y
+    denominator_item1 = n*x_square_sum - squared_sum_x
+    denominator_item2 = n*y_square_sum - squared_sum_y
+    denominator = (denominator_item1*denominator_item2)**0.5
+    correlation = numerator/denominator
+
+    return correlation
 
 
