d7cbf1dc05e21f1bc48bd4df255edbfdd7b306f5
humuhimi <masa19951009@gmail.com>
2018-12-22 13:21:49 +0900

    try4_1 was fixed

--- mathPython/Chapter4/try4_1.py
+++ mathPython/Chapter4/try4_1.py
@@ -8,12 +8,12 @@ from sympy import Symbol, pprint, init_printing
 
 def print_series(n):
     # 次数の低い順から並べる
-    init_printing(order='rev-lex')
+    # init_printing(order='rev-lex')
     x = Symbol('x')
     series = x
     for i in range(2, n+1):
         series = series + (x**i)/i
-    pprint(series)
+    pprint(series,order='rev-lex')
 
 
 if __name__ == '__main__':
