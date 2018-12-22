98d6a14ca7e6c54a7cc462dec76526d0cbcf4e2d
humuhimi <masa19951009@gmail.com>
2018-12-22 13:40:25 +0900

    try4_1 was fixed

--- mathPython/Chapter4/try4_1.py
+++ mathPython/Chapter4/try4_1.py
@@ -6,7 +6,7 @@ and calculate it's value
 from sympy import Symbol, pprint, init_printing
 
 
-def print_series(n):
+def print_series(n, x_value):
     # 次数の低い順から並べる
     # init_printing(order='rev-lex')
     x = Symbol('x')
@@ -14,11 +14,15 @@ def print_series(n):
     for i in range(2, n+1):
         series = series + (x**i)/i
     pprint(series,order='rev-lex')
+    series_value = series.subs({x:x_value})
+    args = "個数:{0},級数:{1},計算結果:{2}"
+    print(args.format(n,x_value,series_value))
 
 
 if __name__ == '__main__':
-    n = int(input('数字を入れてください'))
-    print_series(n)
+    n = int(input('個数を入れてください'))
+    x = float(input('級数を入力してください'))
+    print_series(n,x)
 
 
 
