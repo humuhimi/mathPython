228d67e8ca7a76d621fa8e24cacdec96a0b669cd
humuhimi <masa19951009@gmail.com>
2019-01-02 19:12:15 +0800

    try 5.2

--- mathPython/Chapter5/try5_1.py
+++ mathPython/Chapter5/try5_1.py
@@ -1,16 +1,23 @@
 from sympy import FiniteSet,pi
 
 
-def time_period(length):
-    g = 9.8
+def time_period(length,g):
+    # g = 9.8
     T = 2*pi*(length/g)**0.5
     return T
 
 
 if __name__ == '__main__':
     L = FiniteSet(15,18,21,22.5,25)
-    for l in L:
-        t = time_period(l/100)
-        print('length: {0} cm time period:{1:3f}'.format(float(l),float(t)))
+    g_values = FiniteSet(9.8, 9.7, 9.83)
+    print(('{0:^15}{1:^15}{2:^15}').format('Length(cm)','Gravity(m/s^2)', 'Time Period(s)'))
+
+    for elem in L*g_values:
+        l = elem[0]
+        g = elem[1]
+        t = time_period(l/100, g)
+        # t = time_period(/100)
+        print('{0:^15}{1:^15}{2:^15.3f}'.format(float(l),float(g),float(t)))
+
 
 
