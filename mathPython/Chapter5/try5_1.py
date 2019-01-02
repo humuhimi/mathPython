f0dc9e3ed700df1ca9f7304bfff92bf22bf30220
humuhimi <masa19951009@gmail.com>
2019-01-02 15:35:42 +0800

    try 5.1.3.3

--- mathPython/Chapter5/try5_1.py
+++ mathPython/Chapter5/try5_1.py
@@ -1,1 +1,16 @@
+from sympy import FiniteSet,pi
+
+
+def time_period(length):
+    g = 9.8
+    T = 2*pi*(length/g)**0.5
+    return T
+
+
+if __name__ == '__main__':
+    L = FiniteSet(15,18,21,22.5,25)
+    for l in L:
+        t = time_period(l/100)
+        print('length: {0} cm time period:{1:3f}'.format(float(l),float(t)))
+
 
