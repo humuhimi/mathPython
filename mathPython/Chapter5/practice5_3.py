52517fada74d0389e594836800dd14b9e957a0c3
humuhimi <masa19951009@gmail.com>
2019-01-04 22:39:47 +0800

    practice 5.4

--- mathPython/Chapter5/practice5_3.py
+++ mathPython/Chapter5/practice5_3.py
@@ -3,17 +3,20 @@ import random
 
 def money_game(money):
 
-    stock = int(money)
+    stock = money
     coin = {'表': 1, '裏': 0}
     coin_list = list(coin.keys())
+    toss = random.randint(0,1)
+    win_ammount = 1
+    loss_ammount = 1.5
 
     while stock > 0:
 
-        if coin['表'] == random.randint(0,1):
-            stock += 1
+        if coin['表'] == toss:
+            stock += win_ammount
             result = coin_list[0]
         else: # 裏なら
-            stock -= 1.5
+            stock -= loss_ammount
             result = coin_list[1]
 
         print('{0}です!残金:{1}'.format(result, stock))
@@ -22,7 +25,7 @@ def money_game(money):
 
 if __name__ == '__main__':
 
-    money = input('enter your first amount')
+    money = float(input('enter your first amount'))
     money_game(money)
 
 
