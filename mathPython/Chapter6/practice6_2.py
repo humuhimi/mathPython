7e722b8bfb1d8eea32b07152fea6f58d86d13e75
humuhimi <masa19951009@gmail.com>
2019-01-10 08:42:06 +0800

    practice6.2

--- mathPython/Chapter6/practice6_2.py
+++ mathPython/Chapter6/practice6_2.py
@@ -1,15 +1,5 @@
 import matplotlib.pyplot as plt
-import math
 import random
-"""
-
-変換の確率はどれも同じ=1/3
-シェルピンスキー関数で1/3の確率で変換のどれかを読み出す。
-その値を記録する。
-開始は(0,0)から2点をまとめるp[0,1]
-pは変化する点
-x,yは点の記録
-"""
 
 
 def transform_1(p):
@@ -41,24 +31,24 @@ def transform_3(p):
 
 
 def sierpinski_gasket(n):
-
-    p[0] = 0
-    p[1] = 0
+    transformations = [transform_1, transform_2, transform_3]
+    # p[0] = 0
+    # p[1] = 0
     x = [0]
     y = [0]
 
-    transformations = [transform_1,transform_2,transform_3]
     x1, y1 = 0,0
-    rand_index = random.randint(0,2)
 
     while n > 0:
+        rand_index = random.randint(0, 2)
         t = transformations[rand_index]
-        p = t(p)
-        x.append(p[0])
-        y.append(p[1])
+        p = (x1, y1)
+        x1, y1 = t(p)
+        x.append(x1)
+        y.append(y1)
         n -= 1
 
-    return x,y
+    return x, y
 
 
 if __name__ == '__main__':
